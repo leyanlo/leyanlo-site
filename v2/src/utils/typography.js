ed8987dd30fe710a4cd0443cb5830c090b02755f
Leyan Lo <leyanlo@gmail.com>
2018-08-10 05:08:13 -0700

    Increase header line height

--- v2/src/utils/typography.js
+++ v2/src/utils/typography.js
@@ -1,6 +1,9 @@
 import Typography from 'typography';
 import stAnnesTheme from 'typography-theme-st-annes';
 
+// Too squished otherwise
+stAnnesTheme.headerLineHeight = 1.25;
+
 // Override overflow-y: scroll
 // From https://github.com/KyleAMathews/typography.js/issues/154
 stAnnesTheme.overrideStyles = () => ({
