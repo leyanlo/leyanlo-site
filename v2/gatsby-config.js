9fdfc39b5a584bcf7068195dac7293266e4a921d
Leyan Lo <leyanlo@gmail.com>
2018-08-10 01:00:55 -0700

    Update prettier

--- v2/gatsby-config.js
+++ v2/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -13,10 +13,10 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
     },
     'gatsby-plugin-offline',
-    'gatsby-plugin-typography',
-  ],
-}
+    'gatsby-plugin-typography'
+  ]
+};
