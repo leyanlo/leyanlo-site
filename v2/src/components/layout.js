84c996a321444c2ae324f08a7049fc41f5faefa2
Leyan Lo <leyanlo@gmail.com>
2018-08-10 03:30:36 -0700

    Create Container component

--- v2/src/components/layout.js
+++ v2/src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
+import Container from './container';
 
 import Header from './header';
 import './layout.css';
@@ -29,16 +30,16 @@ const Layout = ({ children, data }) => (
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
-          }}
-        >
-          {children}
-        </div>
+        <Container>
+          <div
+            style={{
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0
+            }}
+          >
+            {children}
+          </div>
+        </Container>
       </>
     )}
   />
