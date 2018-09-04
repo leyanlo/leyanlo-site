a00cb3745742be833a5a29033e7677d7da0ca479
Leyan Lo <leyanlo@gmail.com>
2018-09-04 03:22:01 -0700

    Update Software page

--- src/pages/software.jsx
+++ src/pages/software.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+
+import ResumeIcon from '../images/icon-resume.svg';
 
 const Software = () => (
   <Layout>
     <div className="container -main">
       <article className="article">
-        <h1>Hi from the second page</h1>
-        <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
+        <h1>Software engineer</h1>
+        <p>I like building things! My interests include:</p>
+        <ul>
+          <li>Designing coherent UX systems</li>
+          <li>Streamlining build systems and refactoring codebases</li>
+          <li>Building opinionated CSS frameworks</li>
+          <li>Debugging minified code</li>
+          <li>
+            <a
+              href="https://medium.com/@leyanlo/aligning-git-log-into-columns-93e903ed46d8"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Building a prettier git log
+            </a>
+          </li>
+        </ul>
+        <br />
+        <a href="/leyan-lo-resume-2018-03-21.pdf" className="link">
+          <ResumeIcon className="link__icon" />
+          Download my resume
+        </a>
       </article>
     </div>
   </Layout>
