4ee41c38222609872c5c74584e4afcab70f6baa1
Leyan Lo <leyanlo@gmail.com>
2018-09-11 01:54:59 -0700

    Prevent resume link from being full-width. Remove link to git log Medium post

--- src/pages/software.jsx
+++ src/pages/software.jsx
@@ -9,26 +9,17 @@ const Software = () => (
     <div className="container -main">
       <article className="article">
         <h1>Software engineer</h1>
-        <p>I like building things! My interests include:</p>
+        <p>I like making things! My interests include:</p>
         <ul>
           <li>Designing coherent UX systems</li>
           <li>Refactoring codebases</li>
           <li>Streamlining build systems</li>
-          <li>Building opinionated CSS frameworks</li>
           <li>Debugging minified code</li>
-          <li>
-            <a
-              href="https://medium.com/@leyanlo/aligning-git-log-into-columns-93e903ed46d8"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Building a prettier git log
-            </a>
-          </li>
+          <li>Building a prettier git log</li>
         </ul>
         <br />
-        <a href="/leyan-lo-resume-2018-03-21.pdf" className="link">
-          <ResumeIcon className="link__icon" />
+        <a href="/leyan-lo-resume-2018-03-21.pdf">
+          <ResumeIcon className="linkIcon" />
           Download my resume
         </a>
       </article>
