eca4528b0d10af2090ea29324cc26679fe59b3cc
Leyan Lo <leyanlo@gmail.com>
2018-09-14 04:04:35 -0700

    Add beginner solution

---
+++ src/pages/cubing/beginner.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import FileIcon from '../../images/icon-file.svg';
+import Layout from '../../components/layout';
+import Tabs from '../../components/tabs';
+
+const BeginnerPage = () => (
+  <Layout>
+    <Tabs />
+    <div className="container -main">
+      <article className="article">
+        <h1>Beginner solution</h1>
+        <p>
+          I wrote a four page PDF detailing a layer by layer solution of the
+          Rubik’s cube.
+        </p>
+        <a href="/cube-solution.pdf">
+          <FileIcon className="linkIcon" />
+          Download my beginner solution
+        </a>
+        <p>
+          <small>Last updated September 14, 2008.</small>
+        </p>
+      </article>
+    </div>
+  </Layout>
+);
+
+export default BeginnerPage;
