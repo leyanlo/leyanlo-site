6d4e9ba183459322071b91e70ccb47fb3b700e34
Leyan Lo <leyanlo@gmail.com>
2018-09-19 23:36:48 -0700

    Migrate alg cases to json files

--- src/pages/cubing/f2l.jsx
+++ src/pages/cubing/f2l.jsx
@@ -3,71 +3,7 @@ import React from 'react';
 import F2lPattern from '../../components/cubing/f2l-pattern';
 import Layout from '../../components/layout';
 import cubingTabs from './cubing-tabs.json';
-
-const f2lCases = [
-  { id: 1, pattern: 'BB_BB_RB_WR_RR_RR_____Y____', algs: ["U' R' U R U' R2' U R U' R' U", "U2 R2' U R U' R U R2' U"] },
-  {
-    id: 2,
-    pattern: 'BB_BB_WB_BR_RR_RR_____Y____',
-    algs: ["F R F' R' F R2 F' R' F R F'", "F2 R2' F' R' F R' F' R2 F'"]
-  },
-  { id: 3, pattern: 'BB_BB_WB_BR_RR_RR_____Y____', algs: ["U' R U' R' B' R B U2'"] },
-  { id: 4, pattern: 'BB_BB_WR_BB_RR_RR_____Y____', algs: ["F R' F R D R' D' F2"] },
-  { id: 5, pattern: 'BB_BB_BR_RB_RR_RR_____Y____', algs: ["U' R' U R2 U' R2' U R' F R F'"] },
-  { id: 6, pattern: 'BB_BB__RB_BWRR_RR_R___Y____', algs: ["R U' R' U R' F R F'"] },
-  { id: 7, pattern: 'BB_BB__RW_BRRR_RR_B___Y____', algs: ["R' F R F' R U' R' U"] },
-  { id: 8, pattern: 'BB_BB__RR_BBRR_RR_W___Y____', algs: ["U' R U R' F R' F'"] },
-  {
-    id: 9,
-    pattern: 'BB_BB__BB_RWRR_RR_R___Y____',
-    algs: ["R U' R U R U' R2' U", "R U' R U R2' U' R U", "R' F R2 F' R F R F'"]
-  },
-  {
-    id: 10,
-    pattern: 'BB_BB__BW_RRRR_RR_B___Y____',
-    algs: ["R' F R' F' R' F R2 F'", "R' F R' F' R2 F R' F'", "R U' R2' U R' U' R' U"]
-  },
-  {
-    id: 11,
-    pattern: 'BB_BB__BR_RBRR_RR_W___Y____',
-    algs: [
-      "U' R' U R U' R' U R U' R' U",
-      "F R F' R' F R F' R' F R F'",
-      "R' U' R U R' U' R U R' U' R U",
-      "R F R' F' R F R' F' R F R' F'"
-    ]
-  },
-  { id: 12, pattern: 'BB_BB_W__B__RRRRR____BY____', algs: ["F R' F' R F R' F'"] },
-  { id: 13, pattern: 'BB_BBBR__W__RR_RR__R__Y____', algs: ["U' R U R' U' R U"] },
-  { id: 14, pattern: 'BB_BBBW__B__RR_RR__R__Y____', algs: ["U' R' U R U' R' U"] },
-  { id: 15, pattern: 'BB_BB_R__W__RRRRR____BY____', algs: ["F R F' R' F R F'"] },
-  { id: 16, pattern: 'BB_BBBB__R__RR_RR__R__Y____', algs: ["R F R' F' R' U' R U"] },
-  { id: 17, pattern: 'BB_BB_B__R__RRRRR____BY____', algs: ["R' U' R U R F R' F'"] },
-  { id: 18, pattern: 'BB_BB___W__RRRRRR_B__BY____', algs: ["R F R' F'"] },
-  { id: 19, pattern: 'BB_BBB__B__WRR_RR_RR__Y____', algs: ["R' U' R U"] },
-  { id: 20, pattern: 'BB_BB___W__RRRBRR_B__RY____', algs: ["R' F R2 F' R U' R' U"] },
-  { id: 21, pattern: 'BB_BBR__B__WRR_RR_RB__Y____', algs: ["R U' R2' U R' F R F'"] },
-  { id: 22, pattern: 'BB_BBB__W__RRR_RR_BR__Y____', algs: ["R U' R U R' U' R' U"] },
-  { id: 23, pattern: 'BB_BB___B__WRRRRR_R__BY____', algs: ["R' F R' F' R F R F'"] },
-  { id: 24, pattern: 'BB_BB___B__WRRBRR_R__RY____', algs: ["F R' F' R2 U' R' U"] },
-  { id: 25, pattern: 'BB_BBR__W__RRR_RR_BB__Y____', algs: ["U' R U R2' F R F'"] },
-  { id: 26, pattern: 'BB_BB___R__BRRBRR_W__RY____', algs: ["R2' U2 R2' U R U' R U2'"] },
-  { id: 27, pattern: 'BB_BBR__R__BRR_RR_WB__Y____', algs: ["R2' F2 R2 F' R' F R' F2"] },
-  { id: 28, pattern: 'BB_BB___R__BRRRRR_W__BY____', algs: ["F R2 F' R' F R F'"] },
-  { id: 29, pattern: 'BB_BBB__R__BRR_RR_WR__Y____', algs: ["U' R2' U R U' R' U"] },
-  { id: 30, pattern: 'BB_BB___W__RRR_RR_B___YB___', algs: ["R' F R2 F' R' F R2 F'", "R' F R2 F' R2 F R' F'"] },
-  { id: 31, pattern: 'BB_BB___B__WRR_RR_R___Y__R_', algs: ["R U' R2' U R U' R2' U", "R U' R2' U R2' U' R U"] },
-  { id: 32, pattern: 'BB_BB___W__RRR_RR_B___YR___', algs: ["U' R' U"] },
-  { id: 33, pattern: 'BB_BB___B__WRR_RR_R___Y__B_', algs: ["F R F'"] },
-  { id: 34, pattern: 'BB_BB___W__RRR_RR_B___Y__B_', algs: ["R' F R F' R' F R2 F'", "R' F R F' R2 F R' F'"] },
-  { id: 35, pattern: 'BB_BB___B__WRR_RR_R___YR___', algs: ["R U' R' U R U' R2' U", "R U' R' U R2' U' R U"] },
-  { id: 36, pattern: 'BB_BB___W__RRR_RR_B___Y__R_', algs: ["R U' R' U R' U' R' U", "R' F R' F' R U' R' U"] },
-  { id: 37, pattern: 'BB_BB___B__WRR_RR_R___YB___', algs: ["R' F R F' R F R F'", "R U' R U R' F R F'"] },
-  { id: 38, pattern: 'BB_BB___R__BRR_RR_W___Y__R_', algs: ["R2 U' R' U R' U' R U"] },
-  { id: 39, pattern: 'BB_BB___R__BRR_RR_W___YR___', algs: ["R' U' R2 U R' U' R U"] },
-  { id: 40, pattern: 'BB_BB___R__BRR_RR_W___Y__B_', algs: ["R F R2' F' R F R' F'"] },
-  { id: 41, pattern: 'BB_BB___R__BRR_RR_W___YB___', algs: ["R2' F R F' R F R' F'"] }
-];
+import f2lCases from './f2l-cases.json';
 
 const F2LPage = () => (
   <Layout tabs={cubingTabs}>
--- src/pages/cubing/intermediate.jsx
+++ src/pages/cubing/intermediate.jsx
@@ -5,54 +5,7 @@ import Layout from '../../components/layout';
 import OllPattern from '../../components/cubing/oll-pattern';
 import PllPattern from '../../components/cubing/pll-pattern';
 import cubingTabs from './cubing-tabs.json';
-
-const f2lCases = [
-  { pattern: 'BB_BBBB__R__RR_RR__R__Y____', alg: "R F R' F' R' U' R U" },
-  { pattern: 'BB_BB_B__R__RRRRR____BY____', alg: "R' U' R U R F R' F'" },
-  { pattern: 'BB_BB___W__RRRRRR_B__BY____', alg: "R F R' F'" },
-  { pattern: 'BB_BBB__B__WRR_RR_RR__Y____', alg: "R' U' R U" },
-  { pattern: 'BB_BB___W__RRR_RR_B___YR___', alg: "U' R' U" },
-  { pattern: 'BB_BB___B__WRR_RR_R___Y__B_', alg: "F R F'" },
-  { pattern: 'BB_BB___W__RRR_RR_B___YB___', alg: "R' F R2 F' R' F R2 F'" },
-  { pattern: 'BB_BB___B__WRR_RR_R___Y__R_', alg: "R U' R2' U R U' R2' U" },
-  { pattern: 'BB_BB___W__RRR_RR_B___Y__B_', alg: "R' F R F' R' F R2 F'" },
-  { pattern: 'BB_BB___B__WRR_RR_R___YR___', alg: "R U' R' U R U' R2' U" },
-  { pattern: 'BB_BB___W__RRR_RR_B___Y__R_', alg: "R U' R' U R' U' R' U" },
-  { pattern: 'BB_BB___B__WRR_RR_R___YB___', alg: "R' F R F' R F R F'" },
-  { pattern: 'BB_BB___R__BRR_RR_W___Y__R_', alg: "R2 U' R' U R' U' R U" },
-  { pattern: 'BB_BB___R__BRR_RR_W___YR___', alg: "R' U' R2 U R' U' R U" },
-  { pattern: 'BB_BB___R__BRR_RR_W___Y__B_', alg: "R F R2' F' R F R' F'" },
-  { pattern: 'BB_BB___R__BRR_RR_W___YB___', alg: "R2' F R F' R F R' F'" },
-  { pattern: 'BB_BB_BR_RB_RR_RR_____Y____', alg: "U' R' U R2 U' R2' U R' F R F'" }
-];
-
-const ollEdgeCases = [
-  { pattern: 'LUULFRLDD', alg: "F R U R' U' S R U R' U' f'" },
-  { pattern: 'LUFFFFLDF', alg: "F R U R' U' F'" },
-  { pattern: 'FFFFFRDDD', alg: "R' U' F' U F R" }
-];
-
-const ollCornerCases = [
-  { pattern: 'LFRFFFLFR', alg: "R' U' R U' R' U R U' R' U2 R" },
-  { pattern: 'LFUFFFLFD', alg: "R U2' R2' U' R2 U' R2' U2' R" },
-  { pattern: 'FFUFFFFFD', alg: "R B L' B' R' B L B'" },
-  { pattern: 'FFFFFFDFD', alg: "R2 D R' U2 R D' R' U2 R'" },
-  { pattern: 'FFUFFFDFR', alg: "R' U' R U' R' U2 R" },
-  { pattern: 'UFRFFFFFD', alg: "R U R' U R U2' R'" },
-  { pattern: 'LFFFFFFFD', alg: "R B L B' R' B L' B'" }
-];
-
-const pllCornerCases = [
-  { pattern: [[7, 9], [9, 7]], alg: "R' F R' B2 R F' R' B2 R2 U2" },
-  { pattern: [[1, 3], [3, 1], [7, 9], [9, 7]], alg: "L U' R U2' L' U R' L U' R U2' L' U R'" }
-];
-
-const pllEdgeCases = [
-  { pattern: [[2, 6], [6, 4], [4, 2]], alg: "R' U R' U' R' U' R' U R U R2" },
-  { pattern: [[2, 4], [4, 6], [6, 2]], alg: "R2 U' R' U' R U R U R U' R" },
-  { pattern: [[2, 6], [6, 2], [4, 8], [8, 4]], alg: "U R B' R' B F R' F B' R' B R F2'" },
-  { pattern: [[2, 8], [8, 2], [4, 6], [6, 4]], alg: "M2 U' M2 U2' M" }
-];
+import intermediateCases from './intermediate-cases.json';
 
 const IntermediatePage = () => (
   <Layout tabs={cubingTabs}>
@@ -66,7 +19,7 @@ const IntermediatePage = () => (
 
         <h2>First two layers</h2>
         <div className="algGrid">
-          {f2lCases.map(f2lCase => (
+          {intermediateCases.f2l.map(f2lCase => (
             <div className="algGrid__item" key={f2lCase.pattern}>
               <F2lPattern pattern={f2lCase.pattern} />
               <b className="algGrid__algs">{f2lCase.alg}</b>
@@ -76,7 +29,7 @@ const IntermediatePage = () => (
 
         <h2>Orient last layer edges</h2>
         <div className="algGrid">
-          {ollEdgeCases.map(ollCase => (
+          {intermediateCases.ollEdges.map(ollCase => (
             <div className="algGrid__item" key={ollCase.pattern}>
               <OllPattern pattern={ollCase.pattern} />
               <b className="algGrid__algs">{ollCase.alg}</b>
@@ -86,7 +39,7 @@ const IntermediatePage = () => (
 
         <h2>Orient last layer corners</h2>
         <div className="algGrid">
-          {ollCornerCases.map(ollCase => (
+          {intermediateCases.ollCorners.map(ollCase => (
             <div className="algGrid__item" key={ollCase.pattern}>
               <OllPattern pattern={ollCase.pattern} />
               <b className="algGrid__algs">{ollCase.alg}</b>
@@ -96,7 +49,7 @@ const IntermediatePage = () => (
 
         <h2>Permute last layer corners</h2>
         <div className="algGrid">
-          {pllCornerCases.map(pllCase => (
+          {intermediateCases.pllCorners.map(pllCase => (
             <div className="algGrid__item" key={pllCase.pattern}>
               <PllPattern pattern={pllCase.pattern} />
               <b className="algGrid__algs">{pllCase.alg}</b>
@@ -106,7 +59,7 @@ const IntermediatePage = () => (
 
         <h2>Permute last layer edges</h2>
         <div className="algGrid">
-          {pllEdgeCases.map(pllCase => (
+          {intermediateCases.pllEdges.map(pllCase => (
             <div className="algGrid__item" key={pllCase.pattern}>
               <PllPattern pattern={pllCase.pattern} />
               <b className="algGrid__algs">{pllCase.alg}</b>
