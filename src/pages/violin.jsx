f758c2c6428b3000d6738ae54e127811fb40d171
Leyan Lo <leyanlo@gmail.com>
2018-09-21 00:35:21 -0700

    Add placeholder icon for loading YouTube video

--- src/pages/violin.jsx
+++ src/pages/violin.jsx
@@ -9,14 +9,16 @@ const ViolinPage = () => (
         <h1>Amateur violinist</h1>
         <p>I enjoy playing chamber music with friends, and sometimes I fall asleep at concerts.</p>
         <figure className="article__figure">
-          <iframe
-            title="Pinky Quartet plays Brahms Quartet in G minor, Op. 25 (iv. Rondo alla Zingarese)"
-            className="article__youtube"
-            src="https://www.youtube-nocookie.com/embed/P_HpJasVeT4?rel=0"
-            frameBorder="0"
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-          />
+          <div className="article__youtubeContainer">
+            <iframe
+              title="Pinky Quartet plays Brahms Quartet in G minor, Op. 25 (iv. Rondo alla Zingarese)"
+              className="article__youtube"
+              src="https://www.youtube-nocookie.com/embed/P_HpJasVeT4?rel=0"
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+            />
+          </div>
           <figcaption className="article__figcaption">2017 SLSQ Seminar at Stanford</figcaption>
         </figure>
       </article>
